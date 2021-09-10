fb35b76017293447532518e24b6f0ef893a2f57f
ronaldo suarez <ronaldo3055@gmail.com>
2021-09-10 18:17:06 -0400

    SecurityQuestion and Question entity relationship is wrong. Will fix.

--- src/main/java/com/swe/lawnwebapp/models/SecurityQuestion.java
+++ src/main/java/com/swe/lawnwebapp/models/SecurityQuestion.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 public class SecurityQuestion {
+    // TODO: OneToOne Relationship for Security Qeustion & Question
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
